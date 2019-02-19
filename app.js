d3c881e95231880715a93492e6207bc2a78a8406
Joe Denny <josephpauldenny@gmail.com>
2019-02-19 17:13:39 -0500

    create env variable for DATABASEURL

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var express     = require("express"),
 //requiring routes
 var courseRoutes     = require("./routes/courses"),
     indexRoutes      = require("./routes/index")
-    
-// mongoose.connect("mongodb://localhost/collabo_v1", { useMongoClient: true });
-mongoose.connect("mongodb://Joe:projectM90@collabo-db-shard-00-00-htm2b.gcp.mongodb.net:27017,collabo-db-shard-00-01-htm2b.gcp.mongodb.net:27017,collabo-db-shard-00-02-htm2b.gcp.mongodb.net:27017/test?ssl=true&replicaSet=collabo-db-shard-0&authSource=admin&retryWrites=true");
+
+mongoose.connect(process.env.DATABASEURL, { useMongoClient: true });
+// mongoose.connect("mongodb://Joe:projectM90@collabo-db-shard-00-00-htm2b.gcp.mongodb.net:27017,collabo-db-shard-00-01-htm2b.gcp.mongodb.net:27017,collabo-db-shard-00-02-htm2b.gcp.mongodb.net:27017/collabo_v1?ssl=true&replicaSet=collabo-db-shard-0&authSource=admin&retryWrites=true");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
